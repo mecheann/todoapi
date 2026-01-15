29eb085cc44bc7f2111a0a333c260edf97bb5c91
Meche-Ann Natasia McKenzie <7514098+mecheann@users.noreply.github.com>
2026-01-15 16:24:34 -0500

    -added api project and base structure

---
+++ Mnm.Todo.Api/Domain/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace Mnm.Todo.Api.Domain
+{
+    public class Product
+    {
+        private Product() { }
+
+        public Product(string name, string description, decimal price)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Description = description;
+            Price = price;
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
